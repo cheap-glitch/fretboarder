73d1d462cde59f3532b3148e448f9f0edcd74220
cheap-glitch <cheap.glitch@gmail.com>
2020-01-14 22:19:25 +0100

    Fix tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,40 @@
 module.exports = {
-	root: true,
-	env: {
+    root: true,
+
+    env: {
 		node: true
 	},
-	'extends': [
+
+    'extends': [
 		'eslint:recommended',
 		'plugin:vue/essential',
 		'plugin:vue/recommended',
 	],
-	'plugins': [
+
+    'plugins': [
 		'smarter-tabs',
 	],
-	rules: {
+
+    rules: {
 		'no-console':  process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
 		'no-mixed-spaces-and-tabs':   ['warn', 'smart-tabs'],
 		'smarter-tabs/smarter-tabs':  'warn',
 	},
-	parserOptions: {
+
+    parserOptions: {
 		parser: 'babel-eslint'
-	}
+	},
+
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)'
+        ],
+        env: {
+          mocha: true
+        }
+      }
+    ]
 }
