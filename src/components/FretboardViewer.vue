ac5fc630a6bbc441544a877b6f1223ed69ae57cf
cheap-glitch <cheap.glitch@gmail.com>
2020-05-05 21:23:04 +0200

    Add basic fretboard viewer wrapper

--- src/components/FretboardViewer.vue
+++ src/components/FretboardViewer.vue
@@ -6,12 +6,14 @@
 <!--{{{ Pug -->
 <template lang="pug">
 
-div.FretboardViewer
+div.FretboardViewer(
+	:style="grid"
+	)
 
 	//- Frets
 	FretboardViewerFret(
 		v-for="fret in frets"
-		:key="'fret--' + fret.number"
+		:key="`fret--${fret.string + 1}--${fret.number}`"
 		)
 
 </template>
@@ -21,15 +23,93 @@ div.FretboardViewer
 <!--{{{ JavaScript -->
 <script>
 
-import { instruments } from '@/modules/music'
+import { instruments, tunings } from '@/modules/music'
+import { getFrets }             from '@/modules/fretboard'
 
 export default {
 	name: 'FretboardViewer',
 
 	computed: {
+		grid()
+		{
+			return { 'grid-template-columns': this.fretMin == 0 ? ['40px', ...this.layout] : this.layout };
+		},
+		layout()
+		{
+			/**
+			 * Compute the size of each fret so that:
+			 *     - size(1) = 3/2
+			 *     - size(n) = 1/2 (n = total number of frets)
+			 *
+			 * Which gives the following function:
+			 *     f(x) = (3n - 1)/(2n - 2) - x/(n - 1)
+			 */
+			const n = this.nbFrets;
+			const c = (3*n - 1)/(2*n - 2);
+
+			// Don't include the open string fret in the flexible layout
+			return Array(this.fretMin == 0 ? (this.nbFrets - 1) : this.nbFrets).keys().map(i => `${c - i/(n - 1)}fr`);
+		},
+		inlays()
+		{
+			// List the frets which can have an inlay (only the 12th is omitted)
+			let frets = ['3', '5', '7', '9', '15', '17', '19', '21'];
+
+			switch (this.nbStrings)
+			{
+				/**
+				 * Bass, ukulele, guitar
+				 * Single inlay in the middle + double inlay at the 12th fret
+				 */
+				case 4:  return ['12-1', '12-3', ...frets.map(f => `${f}-2`)];
+				case 6:  return ['12-2', '12-4', ...frets.map(f => `${f}-3`)];
+
+				/**
+				 * Five-string banjo, seven-string guitar, mandolin, nine-, eleven- string guitar
+				 * Double inlay (close for odd frets, spreaded for the 12th)
+				 */
+				case 5:  return ['12-1', '12-4', ...frets.map(f => `${f}-2`), ...frets.map(f => `${f}-3`)];
+				case 7:  return ['12-2', '12-5', ...frets.map(f => `${f}-3`), ...frets.map(f => `${f}-4`)];
+				case 8:  return ['12-2', '12-6', ...frets.map(f => `${f}-3`), ...frets.map(f => `${f}-5`)];
+				case 9:  return ['12-2', '12-7', ...frets.map(f => `${f}-3`), ...frets.map(f => `${f}-6`)];
+				case 11: return ['12-3', '12-8', ...frets.map(f => `${f}-4`), ...frets.map(f => `${f}-7`)];
+
+				/**
+				 * Ten-string guitar
+				 * Double inlay for odd frets, triple for the 12th
+				 */
+				case 10: return ['12-2', '12-8', ...frets.map(f => `${f}-4`), ...frets.map(f => `${f}-6`), ['12-5']];
+			}
+
+			return [];
+		},
+		displayedFrets()
+		{
+			return this.frets.filter(fret => this.fretMin <= fret.number && fret.number <= this.fretMax);
+		},
+		frets()
+		{
+			return getFrets(this.nbStrings, this.tuningNotes, this.displayedScales);
+		},
+		tuningNotes()
+		{
+			return tunings[this.instrument][this.tuning] || tunings[this.instrument]['standard'];
+		},
 		nbStrings()
 		{
 			return instruments[this.instrument].nbStrings;
+		},
+		nbFrets()
+		{
+			return this.fretMax - this.fretMin + 1;
+		},
+		fretMin()
+		{
+			return this.fretRange[0];
+		},
+		fretMax()
+		{
+			return this.fretRange[1];
 		},
 	},
 }
--- src/modules/fretboard.js
+++ src/modules/fretboard.js
@@ -10,21 +10,23 @@ export const MAX_NB_FRETS = 24;
 /**
  * Generate a list of frets
  */
-export function getFrets(nbStrings, tuning, displayedScales)
+export function getFrets(nbStrings, tuningNotes, displayedScales)
 {
 	// Get the model of each scale
 	const models = displayedScales.map(scale => [0, ...((scale.type == 'scale' ? scales : arpeggios)[scale.name].model)]);
 
 	return Array(nbStrings*MAX_NB_FRETS).keys().map(function(fretIndex)
 	{
-		const fretNumber     = fretIndex % MAX_NB_FRETS;
-		const openStringNote = tuning[Math.floor(fretIndex / MAX_NB_FRETS)];
+		const fretNumber = fretIndex % MAX_NB_FRETS;
+		const fretString = Math.floor(fretIndex / MAX_NB_FRETS);
 
 		return {
 			number: fretNumber,
+			string: fretString,
 			scales: displayedScales.reduce(function(fretScales, scale, index)
 			{
-				const interval = (getInterval(scale.tonality, openStringNote) + fretNumber) % 12;
+				// Compute the interval of the fret note relative to the root note of the scale
+				const interval = (getInterval(scale.tonality, tuningNotes[fretString]) + fretNumber) % 12;
 
 				if (models[index].includes(interval))
 					fretScales.push({ index, interval });
