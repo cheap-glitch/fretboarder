b6f4bdc1cefa0be85b48dda9b95690b2e2418ded
cheap-glitch <cheap.glitch@gmail.com>
2020-05-05 21:46:01 +0200

    Add basic App.vue

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+
+
+<!-- src/App.vue -->
+
+
+<!--{{{ Pug -->
+<template lang="pug">
+
+div.App
+	FretboardViewer
+
+</template>
+<!--}}}-->
+
+
+<!--{{{ JavaScript -->
+<script>
+
+import FretboardViewer from '@/components/FretboardViewer'
+
+export default {
+	name: 'App',
+
+	components: {
+		FretboardViewer,
+	},
+
+}
+
+</script>
+<!--}}}-->
+
+
+<!--{{{ SCSS -->
+<style lang="scss" scoped>
+
+.App {
+}
+
+</style>
+<!--}}}-->
+
+<!--{{{ Global styles -->
+<style lang="scss">
+
+// Load the reset stylesheet
+@use "@cheap-glitch/scss-reset/_reset";
+
+// Load the global styles
+//- @use "@/styles/global";
+
+// Override the styles of the slider component
+//- @use "@/styles/slider";
+
+</style>
+<!--}}}-->
