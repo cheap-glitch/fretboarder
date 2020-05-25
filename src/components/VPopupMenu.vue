28968b45a03fb9a8315ab29de31e5d5eddfff1dd
cheap-glitch <cheap.glitch@gmail.com>
2020-05-25 11:50:08 +0200

    Add popup menu component

--- src/components/VPopupMenu.vue
+++ src/components/VPopupMenu.vue
@@ -7,6 +7,22 @@
 <template lang="pug">
 
 div.VPopupMenu
+	VButton(
+		ref="button"
+
+		:icon="icon"
+		:title="title"
+
+		@click="isOpen = !isOpen"
+		)
+	VPopup(
+		:target="$refs.button ? ($refs.button.$el || false) : false"
+		placement="bottom"
+
+		:is-open="isOpen"
+		v-click-outside="clickOutsideHandler"
+		)
+		slot
 
 </template>
 <!--}}}-->
@@ -17,6 +33,31 @@ div.VPopupMenu
 
 export default {
 	name: 'VPopupMenu',
+
+	props: {
+		icon: {
+			type: [Array, String],
+			required: true,
+		},
+		title: {
+			type: String,
+			default: '',
+		},
+	},
+
+	data() {
+		return {
+			isOpen: false,
+		}
+	},
+
+	methods: {
+		clickOutsideHandler(event)
+		{
+			if (!this.$refs.button.$el.contains(event.target))
+				this.isOpen = false;
+		},
+	},
 }
 
 </script>
