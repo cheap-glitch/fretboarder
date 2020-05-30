42a17fa04166057d976f73a108a77354d65f8f39
cheap-glitch <cheap.glitch@gmail.com>
2020-05-30 22:18:43 +0200

    Add support for positions

--- src/modules/fretboard.js
+++ src/modules/fretboard.js
@@ -11,6 +11,24 @@ import { NB_FRETS, MAX_NB_FRETS } from '@/modules/constants'
  */
 export function getFrets(sequences, tuningNotes, capo)
 {
+	// Compute the boundaries of the displayed position for each sequence
+	const positions = sequences.map(function(seq)
+	{
+		if (seq.position === 0)
+			return null;
+
+		// Get the index of the fret of the root note on the lowest string
+		const rootFret = getInterval(tuningNotes[0], seq.tonality);
+
+		// Get the start of the position
+		const posStart = (rootFret - 1 + 15*(seq.position - 1)) % NB_FRETS;
+
+		return {
+			start: posStart,
+			stop:  (posStart + 5) % NB_FRETS,
+		};
+	});
+
 	// Build an array of frets for each string and flatten them in a single list
 	return tuningNotes.flatMap(function(openStringNote, stringNumber)
 	{
@@ -24,7 +42,7 @@ export function getFrets(sequences, tuningNotes, capo)
 		}));
 
 		// Mark the frets that belong to each sequence
-		sequences.forEach(function(seq)
+		sequences.forEach(function(seq, seqIndex)
 		{
 			// Find the first fret whose note is the root of the sequence
 			const rootFret = frets.findIndex(fret => fret.note == seq.tonality);
@@ -32,6 +50,9 @@ export function getFrets(sequences, tuningNotes, capo)
 			// Loop through the intervals and add the sequence index to the sequences list of their corresponding frets
 			[0, ...models[seq.model].intervals].forEach(function(interval)
 			{
+				// If the sequence is limited to a single position, check that the fret is in its boundaries
+				if (positions[seqIndex] !== null && !isInPosition(rootFret + interval, positions[seqIndex])) return;
+
 				// Modify the current fret and the one 12 half-steps above/below it
 				[rootFret + interval, (rootFret + interval + 12) % NB_FRETS].forEach(function(fret)
 				{
@@ -55,51 +76,21 @@ export function getFrets(sequences, tuningNotes, capo)
 	});
 }
 
-// Compute the boundaries of the displayed position for each sequence
-/*
-const positions = sequences.map(function(seq)
-{
-	if (seq.position == 0)
-		return null;
-
-	// Get the index of the fret of the root note on the lowest string
-	const rootFret = getInterval(tuningNotes[0], seq.tonality);
-
-	// Get the start of the position
-	// const posStart = (rootFret - 1 + 15*(seq.position - 1)) % MAX_NB_FRETS,
-
-	return {
-		start: posStart,
-		// stop:  (posStart + 5) % MAX_NB_FRETS,
-	};
-});
-*/
-
-// Compute the interval of the fret note relative to the root note of the sequence
-// const interval = (getInterval(seq.tonality, openStringNote) + fretNumber) % notes.length;
-
-// @TODO:
-// If the sequence is limited to a single position, check that the fret is in its boundaries
-// if (positions[index] !== null && )
-
 /**
  * Check that a fret is in the boundaries of a position
  */
-/*
 function isInPosition(fretNumber, pos)
 {
-	// const otherFret = (fretNumber + 12) % (MAX_NB_FRETS - 1);
+	const otherFret = (fretNumber + 12) % NB_FRETS;
 
 	return pos.start < pos.stop
 		? (pos.start <= fretNumber && fretNumber < pos.stop) || (pos.start <= otherFret && otherFret < pos.stop)
 		: (pos.start <= fretNumber || fretNumber < pos.stop) || (pos.start <= otherFret || otherFret < pos.stop);
 }
-*/
 
 /**
  * Return the positive number of half-steps between two notes
  */
-/*
 function getInterval(note1, note2)
 {
 	const index1 = notes.indexOf(note1);
@@ -107,4 +98,3 @@ function getInterval(note1, note2)
 
 	return index1 <= index2 ? index2 - index1 : notes.length - (index1 - index2);
 }
-*/
