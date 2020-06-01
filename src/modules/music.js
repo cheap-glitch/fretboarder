8b6cdb4ddb178f2e1cd0f2ffba3809a99a393ab2
cheap-glitch <cheap.glitch@gmail.com>
2020-06-01 19:53:46 +0200

    Supplement position definitions

--- src/modules/music.js
+++ src/modules/music.js
@@ -69,10 +69,10 @@ export const models = {
 	'minb':         { intervals: [3,  5,  6,  7, 10],         name: 'blues minor'               , positions: [[ 0, 3], [2, 6], [4, 8], [7, 11], [9, 13]] },
 
 	// Naturals
-	'maj':          { intervals: [2,  4,  5,  7,  9, 11],     name: 'natural major'             , positions: [[ 0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] },
-	'min':          { intervals: [2,  3,  5,  7,  8, 10],     name: 'natural minor'             , positions: [[ 0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] },
-	'harm':         { intervals: [2,  3,  5,  7,  8, 11],     name: 'harmonic minor'            , positions: [[ 0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] },
-	'mel':          { intervals: [2,  3,  5,  7,  9, 11],     name: 'melodic minor'             , positions: [[ 0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] },
+	'maj':          { intervals: [2,  4,  5,  7,  9, 11],     name: 'natural major'             , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [8, 12]] },
+	'min':          { intervals: [2,  3,  5,  7,  8, 10],     name: 'natural minor'             , positions: [[-1, 3], [2, 6], [4, 8], [7, 10], [9, 13]] },
+	'harm':         { intervals: [2,  3,  5,  7,  8, 11],     name: 'harmonic minor'            , positions: [[-1, 3], [2, 6], [3, 7], [6, 10], [9, 13]] },
+	'mel':          { intervals: [2,  3,  5,  7,  9, 11],     name: 'melodic minor'             , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [9, 13]] },
 
 	// Modes
 	'dor':          { intervals: [2,  3,  5,  7,  9, 10],     name: 'dorian'                    },
