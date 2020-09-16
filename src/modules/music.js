45e537db9a3c873121157dbd19a960294583c9e0
cheap-glitch <cheap.glitch@gmail.com>
2020-09-16 15:29:33 +0200

    ✨ style: Fix name of map keys for blues scales

--- src/modules/music.js
+++ src/modules/music.js
@@ -65,8 +65,8 @@ export const models = {
 	// Pentatonics & blues
 	'maj5':         { intervals: [2,  4,  7,  9],             name: 'major pentatonic'          , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [9, 12]] },
 	'min5':         { intervals: [3,  5,  7, 10],             name: 'minor pentatonic'          , positions: [[ 0, 3], [2, 6], [4, 8], [7, 11], [9, 13]] },
-	'majb':         { intervals: [2,  3,  4,  7,  9],         name: 'blues major'               , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [9, 12]] },
-	'minb':         { intervals: [3,  5,  6,  7, 10],         name: 'blues minor'               , positions: [[ 0, 3], [2, 6], [4, 8], [7, 11], [9, 13]] },
+	'bmaj':         { intervals: [2,  3,  4,  7,  9],         name: 'blues major'               , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [9, 12]] },
+	'bmin':         { intervals: [3,  5,  6,  7, 10],         name: 'blues minor'               , positions: [[ 0, 3], [2, 6], [4, 8], [7, 11], [9, 13]] },
 
 	// Naturals
 	'maj':          { intervals: [2,  4,  5,  7,  9, 11],     name: 'natural major'             , positions: [[-1, 3], [1, 5], [3, 7], [6, 10], [8, 12]] },
