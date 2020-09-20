147413e33035eb726ff1124bb950bb3a903110ee
cheap-glitch <cheap.glitch@gmail.com>
2020-09-20 14:21:27 +0200

    🚩 test: Add a test for every scale

--- test/music.test.js
+++ test/music.test.js
@@ -8,16 +8,113 @@ import { models } from '../src/modules/music.js'
 
 describe("scales", () => {
 
-	describe("pentatonics & blues", () => {
+	describe("pentatonics", () => {
+
+		// Source: https://en.wikipedia.org/wiki/Pentatonic_scale
+		testModel('maj5',  'C D E G A C');
+		testModel('min5',  'C E♭ F G B♭ C');
+		testModel('bmaj5', 'C D F G A C');
+		testModel('bmin5', 'C E♭ F A♭ B♭ C');
+		testModel('egyp',  'C D F G B♭ C');
+
+		// Source: https://en.wikipedia.org/wiki/In_scale
+		testModel('in',    'D Eb G A Bb D', 'D');
+		// Source: https://en.wikipedia.org/wiki/Hiraj%C5%8Dshi_scale
+		testModel('hira',  'C-E-F♯-G-B');
+		// Source: https://en.wikipedia.org/wiki/Iwato_scale
+		testModel('iwa',   'D-E♭-G-A♭-C', 'D');
+		// Source: https://en.wikipedia.org/wiki/Insen_scale
+		testModel('ins',   'D-E♭-G-A-C', 'D');
 	});
 
-	describe("naturals", () => {
+	describe("hexatonics", () => {
+
+		// Source: https://en.wikipedia.org/wiki/C_(musical_note)
+		testModel('maj6', 'C D E F G A C');
+		testModel('min6', 'C D E♭ F G A♭ C');
+
+		// Source: https://en.wikipedia.org/wiki/Blues_scale
+		testModel('bmaj',  'C D Eb E G A C');
+		testModel('bmin',  'C E♭ F F# G B♭ C');
+
+		// Source: https://en.wikipedia.org/wiki/Hexatonic_scale
+		testModel('aug',   'C D♯ E G A♭ B C');
+		testModel('wtone', 'C D E F♯ G♯ A♯ C');
+		testModel('tri',   'C D♭ E G♭ G B♭');
+		testModel('2tri',  'C D♭ D F♯ G A♭');
+		testModel('prom',  'C D E F♯ A B♭');
 	});
 
-	describe("modes", () => {
+	describe("heptatonics", () => {
+
+		// Source: https://en.wikipedia.org/wiki/C_(musical_note)
+		testModel('ion',      'C D E F G A B C');
+		testModel('dor',      'C D E♭ F G A B♭ C');
+		testModel('phry',     'C D♭ E♭ F G A♭ B♭ C');
+		testModel('lyd',      'C D E F♯ G A B C');
+		testModel('mix',      'C D E F G A B♭ C');
+		testModel('aeo',      'C D E♭ F G A♭ B♭ C');
+		testModel('locr',     'C D♭ E♭ F G♭ A♭ B♭ C');
+
+		// Source: https://en.wikipedia.org/wiki/Harmonic_major_scale
+		testModel('harmaj',   'C D E F G Ab B C');
+		// Source: https://en.wikipedia.org/wiki/Double_harmonic_scale
+		testModel('dharmaj',  'C Db E F G Ab B C');
+		// Source: https://en.wikipedia.org/wiki/C_(musical_note)
+		testModel('harmmin',  'C D E♭ F G A♭ B C');
+		// Source: https://en.wikipedia.org/wiki/Hungarian_minor_scale
+		testModel('dharmmin', 'C D Eb F# G Ab B C');
+		// Source: https://en.wikipedia.org/wiki/C_(musical_note)
+		testModel('amel',     'C D E♭ F G A B C');
+		testModel('dmel',     (a => { a.reverse(); return a; })('C B♭ A♭ G F E♭ D C'.split(' ')).join(' '));
+
+		// Source: https://en.wikipedia.org/wiki/Jazz_scale#Modes_of_the_melodic_minor_scale
+		testModel('dorf2',    'C–D♭–E♭–F–G–A–B♭');
+		testModel('lydaug',   'C–D–E–F♯–G♯–A–B');
+		testModel('lyddom',   'C–D–E–F♯–G–A–B♭');
+		testModel('mixf6',    'C–D–E–F–G–A♭–B♭');
+		testModel('locrs2',   'C–D–E♭–F–G♭–A♭–B♭');
+		testModel('alt',      'C–D♭–E♭–F♭–G♭–A♭–B♭');
+		// Source: https://en.wikipedia.org/wiki/Phrygian_dominant_scale
+		testModel('phrydom',  'C – D♭ – E – F – G – A♭ – B♭ – C');
+		// Source: https://en.wikipedia.org/wiki/Double_harmonic_scale#Modes
+		testModel('ultphry',  'C Db Eb Fb G G# A C');
+		// Source: https://en.wikipedia.org/wiki/Aeolian_dominant_scale
+		testModel('aeodom',   'C, D, E, F, G, A♭, B♭');
+
+		// Source: https://en.wikipedia.org/wiki/Neapolitan_scale
+		testModel('majneap',  'C  D♭  E♭ F  G  A  B  C');
+		testModel('minneap',  'C  D♭  E♭ F  G  A♭ B  C');
+		// Source: https://en.wikipedia.org/wiki/Persian_scale
+		testModel('pers',     'C, D♭, E, F, G♭, A♭, B, C');
+		// Source: https://en.wikipedia.org/wiki/Hungarian_major_scale
+		testModel('hungmaj',  'C D♯ E F♯ G A B♭');
+		// Source: https://en.wikipedia.org/wiki/Hungarian_minor_scale
+		testModel('hungmin',  'C D Eb F# G Ab B C');
+		testModel('gypsy',    'C D Eb F# G Ab Bb C');
+		// Source: https://en.wikipedia.org/wiki/Ukrainian_Dorian_scale
+		testModel('ukrdor',   'C D E♭ F♯ G A B♭');
+		// Source: https://en.wikipedia.org/wiki/Enigmatic_scale
+		testModel('eni',      'C, D♭, E, F♯, G♯, A♯, B, C');
 	});
 
-	describe("others", () => {
+	describe("octatonics", () => {
+
+		// Source: https://en.wikipedia.org/wiki/Octatonic_scale
+		testModel('dimhw',      'C C# D# E F# G A Bb C');
+		testModel('dimwh',      'C D Eb F F# G# A B C');
+
+		// Source: https://en.wikipedia.org/wiki/Bebop_scale
+		testModel('bebdom',     'C D E F G A Bb B C');
+		testModel('bebmaj',     'C D E F G G# A B C');
+		testModel('bebdor',     'C D Eb E F G A Bb C');
+		testModel('bebdoralt',  'C D Eb F G A Bb B C');
+		testModel('bebhmin',    'C D Eb F G Ab Bb B C');
+		testModel('bebmel',     'C D  Eb F G G# A B C');
+	});
+
+	describe("chromatic", () => {
+		testModel('chro', 'C C♯ D D♯ E F F♯ G G♯ A B♭ B C');
 	});
 });
 
