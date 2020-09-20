3a64396f597a8ccca0ea069eed8e00d1e61423c1
cheap-glitch <cheap.glitch@gmail.com>
2020-09-20 14:20:06 +0200

    ♻️  refactor: Remove useless object helper

--- src/modules/object.js
+++ src/modules/object.js
@@ -4,14 +4,6 @@
  */
 
 /**
- * Create a new object with the same values but with keys modified by a callback
- */
-export function mapObjectKeys(object, callback)
-{
-	return Object.fromEntries(Object.entries(object).map(([key, value]) => [callback(key, value), value]));
-}
-
-/**
  * Create a new object with the same keys and the result of a callback as values
  */
 export function mapObjectToObject(object, callback)
