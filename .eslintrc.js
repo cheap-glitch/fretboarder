364afe47d5d47336928625c15133e63f1289ea83
cheap-glitch <cheap.glitch@gmail.com>
2020-09-28 17:59:19 +0200

    🔩 config: Add warning for C-style braces

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,9 +25,9 @@ module.exports = {
 	],
 
 	rules: {
-		'no-console':  process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-
+		'no-console':                process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger':               process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'brace-style':               ['warn', '1tbs', { allowSingleLine: true }],
 		'no-mixed-spaces-and-tabs':  ['warn', 'smart-tabs'],
 		'smarter-tabs/smarter-tabs': 'warn',
 	},
